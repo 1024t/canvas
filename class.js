97fc1424a3d6f211444eda0e2a5584bec08e27c9
wanghongxin <wanghongxin@gmail.com>
2015-07-17 13:17:56 +0800

    function or prototype

--- class.js
+++ class.js
@@ -1,16 +1,37 @@
 ;(function(root,factory){
-	var Class=factory.call(root);
+	Class=factory.call(root);
 }(this,function(){
 	function initialize(){
 
 	}
-	return function(prototype){
-		function Type(){
-			this.initialize.apply(this,arguments);
+	return function(prototype,init){
+		function Class(){
+			if(arguments.length!=0){
+				this.initialize.apply(this,arguments);
+			}
 		}
-		Type.prototype=prototype;
-		prototype.constructor=prototype.super=Type;
-		prototype.initialize=prototype.initialize||initialize;
-		return  Type;
+		if(typeof prototype==='function'){
+			Class.prototype=new prototype;
+			Class.prototype.constructor=Class;
+			Class.prototype.initialize=init||Class.prototype.initialize;
+			Class.prototype.super=prototype;
+		}else{
+			Class.prototype=prototype;
+			prototype.constructor=Class;
+			prototype.initialize=prototype.initialize||initialize;
+			prototype.super=Object;
+		}
+		return  Class;
 	}
 }));
+var Mt=new Class({
+	type:'Mt',
+	initialize:function(name){
+		this.name=name;
+	}
+});
+var lmt=new Mt('lmt');
+var S=new Class(Mt,function(sex){
+	this.sex=sex;
+});
+var s=new S('female');
